5b33887ad99b4be485b2995bbac0c089a455e4c6
cacahuananche <e1028488@mail.student.tuwien.ac.at>
2014-08-18 15:54:00 +0200

    window resize function

--- Simulator.js
+++ Simulator.js
@@ -16,6 +16,14 @@ function Simulator(){
         vecShaderE, vecShaderB, vecShaderJ;
 
 
+    //corrects scene when window size changes
+    window.addEventListener('resize', onWindowResize,false);
+    function onWindowResize(){
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
     this.init = function(){
 
         initThreejs();
